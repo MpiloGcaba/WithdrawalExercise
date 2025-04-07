217f82ad8e4d04546b5dd93f669618fa97cef6bd
Mpilo <gcabampilo@gmail.com>
2025-04-07 21:40:57 +0200

    fix project name

--- src/main/kotlin/com/example/withdrawalExercise/WithdrawalExerciseApplication.kt
+++ src/main/kotlin/com/example/withdrawalExercise/WithdrawalExerciseApplication.kt
@@ -1,4 +1,4 @@
-package com.example.WithdrawalExercise
+package com.example.withdrawalExercise
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
--- src/test/kotlin/com/example/withdrawalExercise/WithdrawalExerciseApplicationTests.kt
+++ src/test/kotlin/com/example/withdrawalExercise/WithdrawalExerciseApplicationTests.kt
@@ -1,4 +1,4 @@
-package com.example.WithdrawalExercise
+package com.example.withdrawalExercise
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
