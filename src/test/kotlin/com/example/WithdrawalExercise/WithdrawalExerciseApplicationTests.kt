815c2d43bde4d9045d8ed1fdbf6a0163b8d38a9e
Mpilo <gcabampilo@gmail.com>
2025-04-07 21:25:52 +0200

    created project

---
+++ src/test/kotlin/com/example/WithdrawalExercise/WithdrawalExerciseApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.WithdrawalExercise
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WithdrawalExerciseApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
